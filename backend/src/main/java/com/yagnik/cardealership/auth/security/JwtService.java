e16caa77f36c6e81229514dfbac913aee99f7c09
PANSHERIYA YAGNIKKUMAR ASHOKBHAI <yagnikpansheriya1505@gmail.com>
2026-07-12 00:01:50 +0530

    feat(auth): return dummy-token temporary after authentication

--- backend/src/main/java/com/yagnik/cardealership/auth/security/JwtService.java
+++ backend/src/main/java/com/yagnik/cardealership/auth/security/JwtService.java
@@ -1,4 +1,11 @@
 package com.yagnik.cardealership.auth.security;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+
+    public String generateToken(String email) {
+        return "dummy-token";
+    }
 }
--- backend/src/test/java/com/yagnik/cardealership/auth/service/JwtServiceTest.java
+++ backend/src/test/java/com/yagnik/cardealership/auth/service/JwtServiceTest.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import com.yagnik.cardealership.auth.security.*;
+
 public class JwtServiceTest {
 
     @Test
     void shouldGenerateJwtToken() {
 
-        String token = JwtService.generateToken("john@example.com");
+        JwtService jwtService = new JwtService();
+
+        String token = jwtService.generateToken("john@example.com");
 
         assertNotNull(token);
-
         assertFalse(token.isBlank());
     }
 }
