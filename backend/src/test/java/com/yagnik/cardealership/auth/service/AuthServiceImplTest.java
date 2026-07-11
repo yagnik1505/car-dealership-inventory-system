b546cf991d49bf98a3502f918bf9fd58e7cd5aec
PANSHERIYA YAGNIKKUMAR ASHOKBHAI <yagnikpansheriya1505@gmail.com>
2026-07-11 15:35:02 +0530

    test(auth): verify user persistence during registration
    add failing testcase to verify that successful registration persist the user befire returning successful msg.

--- backend/src/test/java/com/yagnik/cardealership/auth/service/AuthServiceImplTest.java
+++ backend/src/test/java/com/yagnik/cardealership/auth/service/AuthServiceImplTest.java
@@ -2,6 +2,7 @@ package com.yagnik.cardealership.auth.service;
 
 import com.yagnik.cardealership.auth.dto.RegisterRequest;
 import com.yagnik.cardealership.auth.dto.RegisterResponse;
+import com.yagnik.cardealership.auth.entity.User;
 import com.yagnik.cardealership.auth.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
@@ -22,7 +24,7 @@ class AuthServiceImplTest {
     private AuthServiceImpl authService;
 
     @Test
-    void shouldRegisterUserSuccessfully() {
+    void shouldSaveUserAndReturnSuccessResponse() {
 
         RegisterRequest request = RegisterRequest.builder()
                 .name("Yagnik")
@@ -38,6 +40,8 @@ class AuthServiceImplTest {
                 "User registered successfully",
                 response.getMessage()
         );
-    }
 
+        verify(userRepository, times(1))
+                .save(any(User.class));
+    }
 }
