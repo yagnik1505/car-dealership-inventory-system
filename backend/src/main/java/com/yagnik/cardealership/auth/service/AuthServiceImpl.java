ccbf9833b645d5565120dd3633845b86ee530ab2
PANSHERIYA YAGNIKKUMAR ASHOKBHAI <yagnikpansheriya1505@gmail.com>
2026-07-11 15:43:31 +0530

    feat(auth): persist user during registration
    Implemented the minimum registration logic to persist a new user.

--- backend/src/main/java/com/yagnik/cardealership/auth/service/AuthServiceImpl.java
+++ backend/src/main/java/com/yagnik/cardealership/auth/service/AuthServiceImpl.java
@@ -2,6 +2,8 @@ package com.yagnik.cardealership.auth.service;
 
 import com.yagnik.cardealership.auth.dto.RegisterRequest;
 import com.yagnik.cardealership.auth.dto.RegisterResponse;
+import com.yagnik.cardealership.auth.entity.Role;
+import com.yagnik.cardealership.auth.entity.User;
 import com.yagnik.cardealership.auth.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,15 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public RegisterResponse register(RegisterRequest request) {
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
 
         return RegisterResponse.builder()
                 .message("User registered successfully")
