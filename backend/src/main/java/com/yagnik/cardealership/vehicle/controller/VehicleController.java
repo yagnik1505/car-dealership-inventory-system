1a61b1bfdbdc1e9cc73489b80c39f01c2d2e8ce9
PANSHERIYA YAGNIKKUMAR ASHOKBHAI <yagnikpansheriya1505@gmail.com>
2026-07-12 10:35:44 +0530

    feat(vehicle): implement get all vehicles endpoint

--- backend/src/main/java/com/yagnik/cardealership/vehicle/controller/VehicleController.java
+++ backend/src/main/java/com/yagnik/cardealership/vehicle/controller/VehicleController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/vehicles")
 @RequiredArgsConstructor
@@ -24,5 +26,11 @@ public class VehicleController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VehicleResponse>> getAllVehicles() {
+
+        return ResponseEntity.ok(vehicleService.getAllVehicles());
     }
 }
